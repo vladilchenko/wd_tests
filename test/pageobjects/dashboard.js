530319d6c51f9a3fecacd17043ded85c3f061e0a
Antonina <antoninasamohval@gmail.com>
2022-11-14 22:51:16 -0500

    check dropdown menu

--- test/pageobjects/dashboard.js
+++ test/pageobjects/dashboard.js
@@ -5,8 +5,43 @@ class DashboardPage extends Page {
         return $('#buzz-content');
     }
 
+    get accountMenu () {
+        //return $('//div[contains(@class,"bellContainer")]');
+        return $('div[class*="bellContainer"]');
+    }
+
+    get dropDownMenu () {
+        return $('#js-user-menu');
+    }
+
+    get menuNewPost() {
+        return this.dropDownMenu.$('//li[contains(.,"New Post")]')
+    }
+
+    get menuMyDraft() {
+        return this.dropDownMenu.$('//li[contains(.,"My Drafts")]')
+    }
+
+    get menuDashboard() {
+        return this.dropDownMenu.$('//li[contains(.,"Dashboard")]')
+    }
+
+    get menulogOut() {
+        return this.dropDownMenu.$('//li[contains(.,"Log Out")]')
+    }
+
+
     async checkUsername (name) {
         await expect(this.username).toHaveText(name);
     }
+
+    async checkMenu () {
+        await expect(this.menuNewPost).toBeExisting();
+        await expect(this.menuMyDraft).toBeExisting();
+        await expect(this.menuDashboard).toBeExisting();
+        await expect(this.menulogOut).toBeExisting();
+    }
+
+
 }
 module.exports = new DashboardPage();
