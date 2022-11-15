98b2ce179688aca02befab2eacb7b1052cd93a72
Antonina <antoninasamohval@gmail.com>
2022-11-14 23:02:20 -0500

    Fixes after code review

--- test/specs/account_menu.js
+++ test/specs/account_menu.js
@@ -3,7 +3,7 @@ const DashboardPage = require('../pageobjects/dashboard');
 
 describe('Account menu', () => {
     it('check menu items', async () => {
-        LoginPage.open('');
+        LoginPage.open();
         await LoginPage.login('kirnybob@gmail.com', 'BuzzFeed123!');
         await DashboardPage.checkUsername('autonya');
         await DashboardPage.accountMenu.click();
--- test/specs/login.js
+++ test/specs/login.js
@@ -3,7 +3,7 @@ const DashboardPage = require('../pageobjects/dashboard');
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        LoginPage.open('');
+        LoginPage.open();
         await LoginPage.login('kirnybob@gmail.com', 'BuzzFeed123!');
         await DashboardPage.checkUsername('autonya');
     });
