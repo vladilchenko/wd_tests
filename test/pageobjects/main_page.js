7bbb8232c37ce6e2ca23c670385912be52741237
Antonina <antoninasamohval@gmail.com>
2022-11-14 21:53:30 -0500

    add first test

---
+++ test/pageobjects/main_page.js
@@ -0,0 +1,16 @@
+const Page = require('./page');
+
+class MainPage extends Page {
+    get quizzesTab () {
+        return $('//nav//a[contains(text(),"Quizzes")]');
+    }
+
+    open() {
+        return super.open("");
+    }
+
+    async open_quizzes_tab () {
+        await this.quizzesTab.click();
+    }
+}
+module.exports = new MainPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,7 +3,7 @@ const SecurePage = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
+        await LoginPage.open('');
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
